9f1ff714683b2bc169e7a86fc7fe38b5f3c3c669
Tavi Nathanson <tavi.nathanson@gmail.com>
2015-02-24 15:40:48 -0500

    Adding setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2014. Mount Sinai School of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+readme_filename = os.path.join(os.path.dirname(__file__), 'README.md')
+with open(readme_filename, 'r') as f:
+  readme = f.read()
+
+try:
+  import pypandoc
+  readme = pypandoc.convert(readme, to='rst', format='md')
+except:
+  print("Conversion of long_description from MD to reStructuredText failed...")
+  pass
+
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='mhctools',
+        version="0.0.0",
+        description="Python interface to running command-line and web-based MHC binding predictors",
+      	author="Alex Rubinsteyn",
+        author_email="alex {dot} rubinsteyn {at} mssm {dot} edu",
+      	url="https://github.com/hammerlab/mhctools",
+        license="http://www.apache.org/licenses/LICENSE-2.0.html",
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Operating System :: OS Independent',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+        ],
+        install_requires=[
+            'numpy>=1.7',
+            'pandas>=0.13.1',
+        ],
+        long_description=readme,
+        packages=['mhctools'],
+    )
