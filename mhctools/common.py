d68c93abb2ca0804d1096325d9e79b463ac438cf
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2015-04-30 18:49:08 -0400

    py3 imports

--- mhctools/common.py
+++ mhctools/common.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function, division, absolute_import
+
 # Python3 doesn't have a unicode classes
 try:
     string_classes = (unicode, str)
