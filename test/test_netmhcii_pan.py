6dc7a1e8434e5473fd571074a55d625514203636
Tavi Nathanson <tavi.nathanson@gmail.com>
2015-08-31 18:30:43 -0400

    Fixing mouse class II alleles

--- test/test_netmhcii_pan.py
+++ test/test_netmhcii_pan.py
@@ -30,3 +30,18 @@ def test_netmhcii_pan_alpha_beta():
 
     assert len(epitope_collection) == 27, \
         "Expected 27 epitopes from %s" % (epitope_collection,)
+
+def test_netmhcii_pan_mouse():
+    alleles = [normalize_allele_name("H2-IAb")]
+    ii_pan_predictor = NetMHCIIpan(
+        alleles=alleles,
+        epitope_lengths=[15, 16])
+    fasta_dictionary = {
+        "SMAD4-001": "PAPAPSWPLSSSVPSQKTYQGSYGFRLGFLHSGT",
+        "TP53-001": "SQAMDDLMLSPDDIEQWFTED"
+    }
+    epitope_collection = ii_pan_predictor.predict(
+        fasta_dictionary=fasta_dictionary)
+
+    assert len(epitope_collection) == 27, \
+        "Expected 27 epitopes from %s" % (epitope_collection,)
