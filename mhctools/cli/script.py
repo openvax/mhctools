669ae656a8aa32196ddf8b938f8e29ca2b62db6e
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2017-03-01 13:56:10 -0500

    got rid of extra import

--- mhctools/cli/script.py
+++ mhctools/cli/script.py
@@ -21,7 +21,6 @@ import sys
 from pyensembl.fasta import parse_fasta_dictionary
 
 from .args import make_mhc_arg_parser, mhc_binding_predictor_from_args
-from ..binding_prediction import binding_predictions_to_dataframe
 
 logging.config.fileConfig(pkg_resources.resource_filename(__name__, 'logging.conf'))
 logger = logging.getLogger(__name__)
