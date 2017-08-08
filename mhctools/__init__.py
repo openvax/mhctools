d08f616257c4b433dffa3a6ed5b54d9464c51b1b
Julia K <julia326@gmail.com>
2017-08-08 10:57:31 -0400

    Version bump

--- mhctools/__init__.py
+++ mhctools/__init__.py
@@ -19,7 +19,7 @@ from .netmhc_pan3 import NetMHCpan3
 from .netmhcii_pan import NetMHCIIpan
 from .random_predictor import RandomBindingPredictor
 
-__version__ = "1.6.4"
+__version__ = "1.6.5"
 
 __all__ = [
     "BindingPrediction",
