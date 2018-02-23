2be6b4385e625ae50f671306e6791bd2e81836e7
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2018-02-23 13:57:18 -0500

    small update to setup and travis

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2016. Mount Sinai School of Medicine
+# Copyright (c) 2014-2018. Mount Sinai School of Medicine
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,15 +23,16 @@ readme_filename = os.path.join(readme_dir, 'README.md')
 
 try:
     with open(readme_filename, 'r') as f:
-        readme = f.read()
+        readme_markdown = f.read()
 except:
     logging.warn("Failed to load %s" % readme_filename)
-    readme = ""
+    readme_markdown = ""
 
 try:
     import pypandoc
-    readme = pypandoc.convert(readme, to='rst', format='md')
+    readme_restructured = pypandoc.convert(readme_markdown, to='rst', format='md')
 except:
+    readme_restructured = readme_markdown
     logging.warn("Conversion of long_description from MD to RST failed")
     pass
 
@@ -72,7 +73,7 @@ if __name__ == '__main__':
             'mhcflurry==1.0.0',
             'mhcnames>=0.3.2',
         ],
-        long_description=readme,
+        long_description=readme_restructured,
         packages=['mhctools', 'mhctools.cli'],
         package_data={'mhctools.cli': ['logging.conf']},
         entry_points={
