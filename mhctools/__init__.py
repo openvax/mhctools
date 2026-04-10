658f2433c712c164363176668ff4c5afe5d83426
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-04-10 15:29:45 -0400

    Bump to 3.5.1 for README/docs update

--- mhctools/__init__.py
+++ mhctools/__init__.py
@@ -41,7 +41,7 @@ from .netmhcstabpan import NetMHCstabpan
 from .bigmhc import BigMHC, BigMHC_EL, BigMHC_IM
 from .unsupported_allele import UnsupportedAllele
 
-__version__ = "3.5.0"
+__version__ = "3.5.1"
 
 __all__ = [
     "Pred",
