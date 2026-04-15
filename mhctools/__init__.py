206fd553737ef0ebb22727428266579478e55326
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-04-15 14:06:00 -0400

    Bump to 3.12.3 for #196 multi-allele NetMHCpan parser fix

--- mhctools/__init__.py
+++ mhctools/__init__.py
@@ -63,7 +63,7 @@ def __getattr__(name):
     raise AttributeError(
         "module %r has no attribute %r" % (__name__, name))
 
-__version__ = "3.12.2"
+__version__ = "3.12.3"
 
 __all__ = [
     "Prediction",
