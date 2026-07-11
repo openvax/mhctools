c7ca5797e8fc36662f11a783a525001bafc5a307
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-07-10 21:37:26 -0400

    Add command-line predictor pair API (#262)

--- mhctools/__init__.py
+++ mhctools/__init__.py
@@ -88,7 +88,7 @@ def __getattr__(name):
     raise AttributeError(
         "module %r has no attribute %r" % (__name__, name))
 
-__version__ = "3.31.4"
+__version__ = "3.31.5"
 
 __all__ = [
     "Prediction",
