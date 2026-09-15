832dbbda8f975294b6b39a0403b87e8f5c911a45
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-09-15 18:34:14 -0400

    Bump to 3.44.17

--- mhctools/__init__.py
+++ mhctools/__init__.py
@@ -125,7 +125,7 @@ def __getattr__(name):
     raise AttributeError(
         "module %r has no attribute %r" % (__name__, name))
 
-__version__ = "3.44.16"
+__version__ = "3.44.17"
 
 __all__ = [
     "Prediction",
