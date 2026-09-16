5f200782bbfd1ef5ea2b264ef01433d1841e8888
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-09-15 23:26:41 -0400

    Bump to 3.44.27

--- mhctools/__init__.py
+++ mhctools/__init__.py
@@ -126,7 +126,7 @@ def __getattr__(name):
     raise AttributeError(
         "module %r has no attribute %r" % (__name__, name))
 
-__version__ = "3.44.26"
+__version__ = "3.44.27"
 
 __all__ = [
     "mhcflurry_composite_version",
