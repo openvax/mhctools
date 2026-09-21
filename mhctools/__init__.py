becd9f7c7acaea937a0c39229974f70414c52b51
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-09-21 15:38:23 -0400

    Preserve IEDB default windows and affinity-only predictions

--- mhctools/__init__.py
+++ mhctools/__init__.py
@@ -144,7 +144,7 @@ def __getattr__(name):
     raise AttributeError(
         "module %r has no attribute %r" % (__name__, name))
 
-__version__ = "3.44.54"
+__version__ = "3.44.55"
 
 __all__ = [
     "mhcflurry_composite_version",
